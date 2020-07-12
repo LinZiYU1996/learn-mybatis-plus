0c5272ac69c4262b20d5a22dd04b1fee453bcdb0
LinZiYu1996 <2669093302@qq.com>
2020-07-12 09:19:30 +0800

    7-11

---
+++ dynamic_tablename_demo/src/main/java/com/lin/dynamic_tablename_demo/DynamicTablenameDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lin.dynamic_tablename_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicTablenameDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicTablenameDemoApplication.class, args);
+    }
+
+}
---
+++ dynamic_tablename_demo/src/test/java/com/lin/dynamic_tablename_demo/DynamicTablenameDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.dynamic_tablename_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicTablenameDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
